3ddf94874cfdb722470d4ed4c042cec8ebb9f60d
sergge1 <shvozdyk@gmail.com>
2020-02-18 09:17:51 +0200

    dev

--- MMM-MQTTbridge.js
+++ MMM-MQTTbridge.js
@@ -26,10 +26,11 @@ Module.register("MMM-MQTTbridge", {
       interval: 300000,
       topicSubscr: [],
     },
+
     // MQTT -> NOTI rules Dictionary - should be set in this structure within ./dict/mqttDictionary.js
     mqttDictionary: {
-
     },
+
     // NOTIF -> MQTT rules Dictionary - should be set in this structure within ./dict/notiDictionary.js
     notiDictionary: {
       notiHook: [
@@ -57,27 +58,31 @@ Module.register("MMM-MQTTbridge", {
   },
 
   updateMqtt: function (self) {
-    self.sendSocketNotification("MQTT_BRIDGE_CONNECT", { mqttServer: self.config.mqttServer, mqttDictionary: self.config.mqttDictionary, mqttConfig: self.config.mqttConfig });
+    self.sendSocketNotification("MQTT_BRIDGE_CONNECT", { mqttServer: self.config.mqttServer, mqttDictionary: self.config.mqttDictionary, mqttConfig: self.config.mqttConfig }); //request to connect to the MQTT broker
     setTimeout(self.updateMqtt, self.config.mqttConfig.interval, self);
   },
 
   socketNotificationReceived: function (notification, payload) {
     switch (notification) {
+      // START MQTT to NOTI logic
       case "MQTT_MESSAGE_RECEIVED":
-        // START MQTT to NOTI logic
+        //chech whether we accept Native MQTT format only or not. Proceed with NATIVE FORMAT only
         if (this.config.mqttConfig.useMqttBridgeFromatOnly) {
           // substract NOTI ID and NOTI PAYLOAD from the MQTT message Payload and send NOTIFICATION
           var mqttNoti = payload.data.slice(0, payload.data.indexOf(":"));
           var mqttPayload = payload.data.slice(payload.data.indexOf(":") + 1);
           this.sendNotification(mqttNoti, mqttPayload);
           this.sendSocketNotification("LOG", "[MQTT bridge] MQTT -> NOTI issued: " + mqttNoti + ", payload: "+ mqttPayload);
+        //chech whether we accept Native MQTT format only or not. Proceed with ALL FORMATS of MQTT
         } else { //if 
-          // search payload useMqttBridgeFromatOnly is false - search MQTT messages within DICTIONARY
+          // search payload of MQTT messages within MQTT DICTIONARY
           for (var i = 0; i < this.config.mqttDictionary.mqttHook.length; i++) {
             if (payload.data == this.config.mqttDictionary.mqttHook[i].mqttPayload) {
+              // if payload found -> search COMMAND within COMMAND list
               for (var j = 0; j < this.config.mqttDictionary.mqttHook[i].mqttNotiCmd.length; j++) {
-                for (var x in this.config.mqttDictionary.mqttNotiCommands) {
+                for (var x in this.config.mqttDictionary.mqttNotiCommands) { //dictionary rool can reference each MQTT message for several Commands, so lets send NOTI for each Command now
                   if (this.config.mqttDictionary.mqttHook[i].mqttNotiCmd[j] == this.config.mqttDictionary.mqttNotiCommands[x].commandId) {
+                    // send NOTI based on the command specification and also send socketNoti to display the log in terminal
                     this.sendNotification(this.config.mqttDictionary.mqttNotiCommands[x].notiID, this.config.mqttDictionary.mqttNotiCommands[x].notiPayload);
                     this.sendSocketNotification("LOG","[MQTT bridge] MQTT -> NOTI issued: " + this.config.mqttDictionary.mqttNotiCommands[x].notiID + ", payload: "+ this.config.mqttDictionary.mqttNotiCommands[x].notiPayload);
                     break;
@@ -93,7 +98,7 @@ Module.register("MMM-MQTTbridge", {
       case "ERROR":
         this.sendNotification("SHOW_ALERT", payload);
         break;
-      case "DICTIONARIES": //append dictionaries from external files
+      case "DICTIONARIES": //use dictionaries from external files at module sturt-up
         this.config.notiDictionary.notiHook = payload.notiHook;
         this.config.notiDictionary.notiMqttCommands = payload.notiMqttCommands;
         this.config.mqttDictionary.mqttHook = payload.mqttHook;
@@ -104,34 +109,36 @@ Module.register("MMM-MQTTbridge", {
 
   notificationReceived: function (notification, payload, sender) {
     // START of NOTIFICATIONS to MQTT logic
-    if (!this.config.notiConfig.listenNoti) { return; } // check whether do we need to listen for the NOTIFICATIONS. Return if "false"
-    var sndname = "system";
+    if (!this.config.notiConfig.listenNoti) { return; } // check whether we need to listen for the NOTIFICATIONS. Return if "false"
+    var sndname = "system"; //sender name default is "system"
 
-    if (!sender === false) { sndname = sender.name; }; //if no SENDER specified in NOTIFICATION, the SENDER is set to "system" (according to MM documentation), otherwise - use sender name
+    if (!sender === false) { sndname = sender.name; }; //if no SENDER specified in NOTIFICATION, the SENDER is left as "system" (according to MM documentation), otherwise - use sender name
     var self = this;
-    if (this.config.notiConfig.useMqttBridgeFromatOnly) { // check if we are interested in NOTI_TO_MQTT notifications only or not. For saving CPU usage if not unnecessary to check all NOTIs
-      if (notification == "NOTI_TO_MQTT") {
+    if (this.config.notiConfig.useMqttBridgeFromatOnly) { // check if we are interested in Native Noti format "NOTI_TO_MQTT" notifications only or not. Native format saves CPU usage. Native NOTI format: 'NOTI_TO_MQTT:{mqttTopic: "data", mqttPayload: "data"}'
+      if (notification == "NOTI_TO_MQTT") { //so if Native format is TRUE, lets proceed with "NOTI_TO_MQTT" notifications only. This notis have predefined fields it its payload: 'mqttTopic' and 'mqttPayload'.
         this.sendSocketNotification("MQTT_MESSAGE_SEND", {
           mqttServer: self.config.mqttServer,
           topic: payload.mqttTopic,
           payload: payload.mqttPayload,
         });
       }
-      //if useMqttBridgeFromatOnly is false - check all the NOTIFICATIONS
+      //if Native Noti format (useMqttBridgeFromatOnly) usage is false - check all the NOTIFICATIONS
     } else {
-      // exclude NOTIFICATIONS where SENDER in ignore list
+      // exclude NOTIFICATIONS where SENDER in ignored list
       for (var x in this.config.notiConfig.ignoreNotiSender) {
         if (sndname == this.config.notiConfig.ignoreNotiSender[x]) { return; }
       }
-      // exclude NOTIFICATIONS where NOTIFICATION ID in ignore list
+      // exclude NOTIFICATIONS where NOTIFICATION ID in ignored list
       for (var x in this.config.notiConfig.ignoreNotiId) {
         if (notification == this.config.notiConfig.ignoreNotiId[x]) { return; }
       }
-      // search NOTIFICATIONS and PAYLOADS within DICTIONARY
+      // search NOTIFICATIONS and PAYLOADS within DICTIONARY. start with NOTI ID
       for (var i = 0; i < this.config.notiDictionary.notiHook.length; i++) {
         if (this.config.notiDictionary.notiHook[i].notiId === notification) {
+          // continue searching - Payload
           for (var j = 0; j < this.config.notiDictionary.notiHook[i].notiPayload.length; j++) {
             if (this.config.notiDictionary.notiHook[i].notiPayload[j].payloadValue == payload) {
+              // if NOTI ID and Payload found - search for Commands (could be an array of commands)
               for (var k = 0; k < this.config.notiDictionary.notiHook[i].notiPayload[j].notiMqttCmd.length; k++) {
                 for (var x in this.config.notiDictionary.notiMqttCommands) {
                   if (this.config.notiDictionary.notiHook[i].notiPayload[j].notiMqttCmd[k] == this.config.notiDictionary.notiMqttCommands[x].commandId) {
--- node_helper.js
+++ node_helper.js
@@ -10,8 +10,8 @@
 
 const NodeHelper = require('node_helper');
 const mqtt = require('mqtt');
-const { notiHook, notiMqttCommands } = require('./dict/notiDictionary.js');
-const { mqttHook, mqttNotiCommands } = require('./dict/mqttDictionary.js');
+const { notiHook, notiMqttCommands } = require('./dict/notiDictionary.js'); //read the custom NOTI->MQTT rules from external files (they are in a config.notiDictionary )
+const { mqttHook, mqttNotiCommands } = require('./dict/mqttDictionary.js'); //read the custom MQTT->NOTI rules from external files (they are in a config.mqttDictionary )
 
 module.exports = NodeHelper.create({
   start: function () {
@@ -28,12 +28,12 @@ module.exports = NodeHelper.create({
       client = mqtt.connect(config.mqttServer);
       self.clients[config.mqttServer] = client;
 
-      client.on('error', function (error) {
+      client.on('error', function (error) { //MQTT library function. Returns ERROR when connection to the broker could not be established.
         console.log("[MQTT bridge] MQTT brocker error: " + error);
         self.sendSocketNotification('ERROR', { type: 'notification', title: '[MMM-MQTTbridge]', message: 'MQTT broker rised the following error: ' + error });
       });
 
-      client.on('offline', function () {
+      client.on('offline', function () { //MQTT library function. Returns OFFLINE when the client (our code) is not connected.
         console.log("[MQTT bridge] Could not establish connection to MQTT brocker");
         self.sendSocketNotification('ERROR', { type: 'notification', title: '[MMM-MQTTbridge]', message: "MQTT broker can't be reached" });
         client.end();
@@ -42,27 +42,29 @@ module.exports = NodeHelper.create({
       client = self.clients[config.mqttServer];
     }
 
+    // subscription to MQTT topics from config
     for (var i = 0; i < config.mqttConfig.topicSubscr.length; i++) {
       if (!client.connected) {
         client.subscribe(config.mqttConfig.topicSubscr[i]);
         console.log("[MQTT bridge] Subscribed to the topic: " + config.mqttConfig.topicSubscr[i]);
       }
     }
-    if (config.mqttConfig.listenMqtt) {
-      client.on('message', function (topic, message) {
+    if (config.mqttConfig.listenMqtt) { //proceed further if CONFIG listenMqtt is true only.
+      client.on('message', function (topic, message) {  //MQTT library function. Returns message topic/payload when it arrives to subscribed topics.
         console.log('[MQTT bridge] MQTT message received. Topic: ' + topic + ', message: ' + message.toString());
-        self.sendSocketNotification('MQTT_MESSAGE_RECEIVED', { 'topic': topic, 'data': message.toString() });
+        self.sendSocketNotification('MQTT_MESSAGE_RECEIVED', { 'topic': topic, 'data': message.toString() }); // send mqtt mesage payload for further converting to NOTI to MMM-MQTTbridge.js file
       });
     };
   },
 
+  // check all messages arrived from MMM-MQTTbridge.js
   socketNotificationReceived: function (notification, payload) {
     switch (notification) {
-      case 'MQTT_BRIDGE_CONNECT':
+      case 'MQTT_BRIDGE_CONNECT': //case which appear at sturt-up.It sends pre-read arrays of custom dictionaries
         this.connectMqtt(payload);
         this.sendSocketNotification("DICTIONARIES", { "notiHook": notiHook, "mqttHook": mqttHook, "notiMqttCommands": notiMqttCommands, "mqttNotiCommands": mqttNotiCommands });
         break;
-      case 'MQTT_MESSAGE_SEND':
+      case 'MQTT_MESSAGE_SEND': // if this message arrived, commands below send MQTT message using payload information
         var client = this.clients[payload.mqttServer];
         if (typeof client !== "undefined") {
           client.publish(payload.topic, payload.payload);
@@ -70,7 +72,7 @@ module.exports = NodeHelper.create({
         console.log("[MQTT bridge] NOTI->MQTT. Topic: " + payload.topic + ", payload: " + payload.payload);
         break;
       case 'LOG':
-        console.log(payload);
+        console.log(payload); //just to display LOG in Terminal, not console.
         break;
     }
   }
